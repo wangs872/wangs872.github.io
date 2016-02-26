413337ccec1c53b0947581256964ab3c64a32842
wangs872 <wangs872@newschool.edu>
2016-02-25 20:54:03 -0500

    octa

---
+++ Octa site/background.js
@@ -0,0 +1,39 @@
+function red()
+
+{
+
+document.body.style.backgroundColor="red"
+
+}
+
+function blue()
+
+{
+
+document.body.style.backgroundColor="#0c24fb"
+
+}
+
+function green()
+
+{
+
+document.body.style.backgroundColor="green"
+
+}
+
+function yellow()
+
+{
+
+document.body.style.backgroundColor="yellow"
+
+}
+
+function white()
+
+{
+
+document.body.style.backgroundColor="white"
+
+}
